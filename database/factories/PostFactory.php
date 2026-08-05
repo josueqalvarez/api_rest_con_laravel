1a2ccb8917cac424b75ac0d63f22bcba1a5ee99d
josueqalvarez <quichcajosue@gmail.com>
2026-08-05 15:03:19 -0500

    api_rest_con_laravel

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => $this->faker->numberBetween(1, 10),
+            'title' => $this->faker->sentence(),
+            'slug' => $this->faker->unique()->slug(),
+            'content' => $this->faker->paragraph(10),
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory(10)->create();
+
+        Post::factory(120)->create();
+    }
+}
